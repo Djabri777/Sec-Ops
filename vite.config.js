9fab104f96f6088dd5c452f7858369cd32496481
Djabri777 <gabiselt777@gmail.com>
2026-02-18 23:09:08 +0100

    Force-refreshing the CDN

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sec-Ops-version-1',
+  base: '/Sec-Ops-version-1/',
   server: {
     port: 3000,
     open: true
